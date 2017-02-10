3e8537283647c71595cf78dc2bcf029d6f7a0a3f
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-02-10 17:02:29 +0300

    Create an app layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Maoni App</title>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.3.1/css/bulma.min.css">
+</head>
+<body>
+
+    @yield('content')
+
+</body>
+</html>
