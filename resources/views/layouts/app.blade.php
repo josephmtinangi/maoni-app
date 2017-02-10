dbbfe73d4096c7713a0435d37ac543e6499ad5d0
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-02-10 18:49:03 +0300

    Add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Maoni App</title>
+    <title>{{ Config('app.name') }}</title>
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.3.1/css/bulma.min.css">
     <link rel="stylesheet" type="text/css" href="{{ url('/css/app.css') }}">
@@ -13,7 +13,9 @@
     </div>
 
     @yield('content')
+    
+    @include('partials.footer')
 		
-		<script src="{{ url('/js/app.js') }}"></script>
+	<script src="{{ url('/js/app.js') }}"></script>
 </body>
 </html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="footer">
+  <div class="container">
+    <div class="content has-text-centered">
+      <p>
+        <strong>{{ Config('app.name') }}</strong> by <a href="https://josephmtinangi.github.io">Joseph Mtinangi</a>.
+      </p>
+      <p>
+        <a class="icon" href="https://www.bitbucket.org/josephmtinangi">
+          <i class="fa fa-fw fa-bitbucket"></i>
+        </a>      
+        <a class="icon" href="https://www.freecodecamp.com/josephmtinangi">
+          <i class="fa fa-fw fa-free-code-camp"></i>
+        </a>      
+        <a class="icon" href="https://github.com/josephmtinangi">
+          <i class="fa fa-fw fa-github"></i>
+        </a>
+      </p>
+    </div>
+  </div>
+</footer>
