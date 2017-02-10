8c7858cb7cbd42add84103ea9866ef9629f3ac32
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-02-10 16:12:38 +0300

    Add bulma hero

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Maoni App</title>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.3.1/css/bulma.min.css">
 </head>
 <body>
-    
+    <section class="hero is-info is-medium">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <h1 class="title">
+                    Maoni yako
+                </h1>
+                <h2 class="subtitle">
+                    Tujenge Taifa Pamoja
+                </h2>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
